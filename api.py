6170594ed261885cc73625249dc7fa5318d7db80
Vivek Verma <vivnps.verma@gmail.com>
2018-12-21 01:42:41 -0800

    Adding API

---
+++ api.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from flask_cors import CORS
+import sqlite3
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/submit")
+def submit():
+    name = request.args.get('name')
+    with sqlite3.connect('data.db') as conn:
+        c = conn.cursor()
+        c.execute("INSERT INTO names VALUES (?)", (name))
+        conn.commit()
+   
