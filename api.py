eeb94c54bd45409072631f358f28513f7dbf7c27
Vivek Verma <vivnps.verma@gmail.com>
2018-12-21 10:06:15 -0800

    Finishing Backend

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 import sqlite3
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -9,7 +10,28 @@ CORS(app)
 @app.route("/submit")
 def submit():
     name = request.args.get('name')
-    with sqlite3.connect('data.db') as conn:
-        c = conn.cursor()
-        c.execute("INSERT INTO names VALUES (?)", (name))
-        conn.commit()
+    try:
+        with sqlite3.connect('data.db') as conn:
+            c = conn.cursor()
+            c.execute("INSERT INTO names VALUES (?)", (name,))
+            conn.commit()
+    except sqlite3.Error as c:
+        return str(c)
+    return "Success"
+
+
+@app.route("/get")
+def get():
+    try:
+        with sqlite3.connect('data.db') as conn:
+            c = conn.cursor()
+            c.execute("SELECT * FROM names")
+            ret = list(map(lambda x: str(x[0]), c.fetchall()))
+    except sqlite3.Error:
+        return "Error"
+    print()
+    return "<br>".join(ret)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
